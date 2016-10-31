c1612fc73255aaa6bbf21fe2713bab29178f7f6f
Maxime <mpauloin@gmail.com>
2016-10-31 13:36:10 +0100

    Correction de Forget your password Ajout de la page d'erreur 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Error 404</title>
+    {!! Html::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css')!!}
+    {!! Html::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js')!!}
+</head>
+<body>
+<br>
+<div class="col-sm-offset-4 col-sm-4">
+    <div class="panel panel-danger">
+        <div class="panel-heading">
+            <h3 class="panel-title">404 Error !</h3>
+        </div>
+        <div class="panel-body">
+            <p>We are sorry but the page you are looking for does not exist.</p>
+            <p>Team IT Helpdesk. </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
